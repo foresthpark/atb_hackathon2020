0ce3a258f8ed580994f1edfbe82091cdf0377c5c
Joe Dang <mrjoedang@gmail.com>
2020-02-08 11:45:03 -0700

    chore(page): include skeleton for payment page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:atb_hackathon/views/homepage.dart';
-import 'package:atb_hackathon/views/pending_categories.dart';
 import 'package:atb_hackathon/views/tab_page.dart';
+import 'package:atb_hackathon/views/payment_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,6 +9,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return PendingCategories();
+    return MaterialApp(home: Homepage());
   }
 }
