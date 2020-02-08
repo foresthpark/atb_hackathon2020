625ce86a4f928bd9f37296bbd53b0517939aa786
Joe Dang <mrjoedang@gmail.com>
2020-02-08 12:54:14 -0700

    feat(payment): include payment page with details

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:atb_hackathon/views/homepage.dart';
 import 'package:atb_hackathon/views/tab_page.dart';
-import 'package:atb_hackathon/views/payment_page.dart';
+import 'package:atb_hackathon/views/credit_card/credit_card.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,6 +9,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Homepage());
+    return MaterialApp(home: CreditCardScreen());
   }
 }
